291502bc2e8d9fb28c96aac224311be0dd3c96ea
nikita.dotsenko <nikita.dotsenko@danads.se>
2022-12-07 05:07:13 +0200

    asdf

--- src/Main/Search.java
+++ src/Main/Search.java
@@ -1,15 +1,15 @@
 package Main;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class Search {
     public static void main(String[] args) {
-        int[] sizes = {100};
+        int[] sizes = {100, 1000, 10000};
 //        int keyToSearch = 10 * 11;
 
         for (int size : sizes) {
             int[] array = Main.generateArray(size);
+            Arrays.sort(array);
             int[] comparesCountInterAvg = new int[size];
             int[] iterationCountInterAvg = new int[size];
             int[] foundsCountInterAvg = new int[size];
@@ -19,69 +19,68 @@ public class Search {
             int[] foundsCountBinAvg = new int[size];
 
             int[] forInterpolation = Arrays.copyOf(array, array.length);
-            long interpolationStart = System.currentTimeMillis();
-            for (int i = 0; i <= forInterpolation.length -1; i++) {
+            long interpolationStart = System.nanoTime();
+            for (int i = 0; i <= forInterpolation.length - 1; i++) {
                 int comparesCountInter = 0;
                 int iterationCountInter = 0;
                 int[] interpolationIndex = interpolationSearch(forInterpolation, 0, forInterpolation.length - 1, forInterpolation[i], comparesCountInter, iterationCountInter);
                 comparesCountInterAvg[i] = interpolationIndex[1];
                 iterationCountInterAvg[i] = interpolationIndex[2];
                 if (foundsCountBinAvg[0] != -1) {
-                    System.out.println("inter "+interpolationIndex[0]);
                     foundsCountInterAvg[i] = interpolationIndex[0];
                 }
-//                System.out.println("Index inter: " + interpolationIndex[0]);
             }
-//            System.out.println("comparesCountInterAvg: "+Arrays.toString(comparesCountInterAvg));
-//            System.out.println("iterationCountInterAvg: "+Arrays.toString(iterationCountInterAvg));
-            long interpolationEnd = System.currentTimeMillis();
+            long interpolationEnd = System.nanoTime();
             System.out.println("Interpolation search for average(T): " + size + " took " + (interpolationEnd - interpolationStart));
             int avgComparesInter = Arrays.stream(comparesCountInterAvg).sum() / size;
-            System.out.println("avg compares: "+ avgComparesInter + " inter for size "+ size);
+            System.out.println("avg compares: " + avgComparesInter + " inter for size " + size);
             int avgIterInter = Arrays.stream(iterationCountInterAvg).sum() / size;
-            System.out.println("avg iters: "+ avgIterInter + " inter for size "+ size);
+            System.out.println("avg iters: " + avgIterInter + " inter for size " + size);
 
-            System.out.println("founds: "+ foundsCountInterAvg.length + " inter for size "+ size);
+//            System.out.println("founds: " + foundsCountInterAvg.length + " inter for size " + size);
             System.out.println("---------------------------------------------------------");
 
             // Binary
             int[] forBinary = Arrays.copyOf(array, array.length);
-            long binaryStart = System.currentTimeMillis();
-            for (int j = 0; j <= forBinary.length -1; j++) {
+            long binaryStart = System.nanoTime();
+            for (int j = 0; j <= forBinary.length - 1; j++) {
                 int comparesCountBin = 0;
                 int iterationCountBin = 0;
 
-                int[] binaryIndex = binarySearch(forBinary, 0, forBinary.length - 1, forBinary[j], comparesCountBin, iterationCountBin);
+                int[] binaryIndex = binarySearch(
+                        forBinary,
+                        0,
+                        forBinary.length - 1,
+                        forBinary[j],
+                        comparesCountBin,
+                        iterationCountBin
+                );
+
                 comparesCountBinAvg[j] = binaryIndex[1];
                 iterationCountBinAvg[j] = binaryIndex[2];
-//                System.out.println("Index binary: " + binaryIndex[0]);
+
                 if (foundsCountBinAvg[0] != -1) {
-                    System.out.println("bin: "+foundsCountBinAvg[0]);
                     foundsCountInterAvg[j] = foundsCountBinAvg[0];
                 }
             }
-            long binaryEnd = System.currentTimeMillis();
-//            System.out.println("comparesCountBinAvg: "+Arrays.toString(comparesCountBinAvg));
-//            System.out.println("iterationCountBinAvg: "+Arrays.toString(iterationCountBinAvg));
+            long binaryEnd = System.nanoTime();
             System.out.println("Binary search for average(T): " + size + " took " + (binaryEnd - binaryStart));
             int avgComparesBin = Arrays.stream(comparesCountBinAvg).sum() / size;
-            System.out.println("avg compares: "+ avgComparesBin + " binary for size "+ size);
+            System.out.println("avg compares: " + avgComparesBin + " binary for size " + size);
             int avgIterBin = Arrays.stream(iterationCountBinAvg).sum() / size;
-            System.out.println("avg iters: "+ avgIterBin + " binary for size "+ size);
-            System.out.println("founds: "+ foundsCountBinAvg.length + " inter for size "+ size);
+            System.out.println("avg iters: " + avgIterBin + " binary for size " + size);
+//            System.out.println("founds: " + foundsCountBinAvg.length + " inter for size " + size);
             System.out.println("---------------------------------------------------------");
 
         }
     }
 
-    public static int[] interpolationSearch(int[] arr, int lowest, int highest, int x, int comparesCount, int iterationCount) {
+        public static int[] interpolationSearch(int[] arr, int lowest, int highest, int x, int comparesCount, int iterationCount) {
         int position;
 
         if (lowest <= highest && x >= arr[lowest] && x <= arr[highest]) {
             comparesCount++;
-
-            position = lowest + (((highest - lowest) / (arr[highest] - arr[lowest])) * (x - arr[lowest]));
-
+            position = lowest + ((x - arr[lowest]) * (highest - lowest) / (arr[highest] - arr[lowest]));
             if (arr[position] == x) {
                 comparesCount++;
 
